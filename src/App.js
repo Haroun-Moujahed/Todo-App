48041359da0007bbaf59ce26bcddf9fac0de63f9
Haroun-Moujahed <haroun.moujahed.tu@gmail.com>
2019-11-20 13:23:10 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Creation from "./Components/Creation";
+
+function App() {
+  return (
+    <div className="App">
+      <Creation />
+    </div>
+  );
+}
+
+export default App;
